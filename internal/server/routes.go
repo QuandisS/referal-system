9b3e9e57f4272ccfda9916b3b76dd9705484f2aa
anton.kravtsov.31 <anton.kravtsov.31@gmail.com>
2024-11-02 23:42:56 +0300

    feat: basic auth endpoint and cors

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -7,19 +7,30 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  }))
 
-	r.Get("/", s.HelloWorldHandler)
-	r.Get("/dbhealth", s.healthHandler)
-
+	r.Get("/", s.HandleIndex)
+	r.Get("/dbhealth", s.HandleDbHealth)
+	r.Get("/auth/{provider}", s.HandleProviderLogin)
 	return r
 }
 
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandleIndex(w http.ResponseWriter, r *http.Request) {
 	resp := make(map[string]string)
 	resp["message"] = "Hello World"
 
@@ -31,7 +42,12 @@ func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write(jsonResp)
 }
 
-func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) HandleDbHealth(w http.ResponseWriter, r *http.Request) {
 	jsonResp, _ := json.Marshal(s.db.Health())
 	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) HandleProviderLogin(w http.ResponseWriter, r *http.Request) {
+	// Logic for handling login with Google will go here
+	log.Println("Login with Google clicked")
 }
---
+++ web/front/src/routes/login/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    const handleLogin = async () => {
+        const url = 'http://localhost:8080/auth/google';
+
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }); 
+
+            if (!response.ok) {
+                throw new Error('Failed to login! status: ' + response.status);
+            }
+
+            const data = await response.json();
+            console.log(data);
+
+            } catch (error) {
+            console.error('Error during fetch:', error);
+        }
+    };
+  </script>
+  
+  <div class="flex items-center justify-center h-screen bg-gray-100">
+    <div class="bg-white p-8 rounded shadow-md w-96">
+      <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+      <button 
+        on:click={handleLogin} 
+        class="w-full bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition duration-200">
+        Login with Google
+      </button>
+    </div>
+  </div>
